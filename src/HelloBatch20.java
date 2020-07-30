89abb38aea060c558199e34f868d10a1df53c11e
lukykatie <lukykatie@gmail.com>
2020-07-29 21:57:50 -0400

    first commit

---
+++ src/HelloBatch20.java
@@ -0,0 +1,8 @@
+public class HelloBatch20 {
+
+    public static void main(String[] args) {
+
+        System.out.println("It's my second month of Java and my head is about to explode");
+    }
+}
+git  add.class
