c0bbc5c954a7f361497b011bb966c789ab763166
lukykatie <lukykatie@gmail.com>
2020-08-01 12:36:04 -0400

    update Happy again.java

--- src/HappyAgain.java
+++ src/HappyAgain.java
@@ -1,4 +1,12 @@
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
 public class HappyAgain {
 
-    // i
+    public static void main(String[] args) {
+
+
+        // i
+
+        System.out.println("hi");
+    }
 }
