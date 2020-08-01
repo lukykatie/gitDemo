a376436f06e39a33ef02ee68e1716eea38090675
lukykatie <lukykatie@gmail.com>
2020-08-01 11:52:55 -0400

    add Again message.java

--- src/Happy20.java
+++ src/Happy20.java
@@ -4,7 +4,8 @@ public class Happy20 {
     public static void main(String[] args) {
         System.out.println("B20 is always happy");
 
-
+String str = "Again?";
+        System.out.println(str);
 
 
     }
--- src/HelloBatch20.java
+++ src/HelloBatch20.java
@@ -3,5 +3,9 @@ public class HelloBatch20 {
     public static void main(String[] args) {
 
         System.out.println("It's my second month of Java and my head is about to explode");
+
+    String hello ="Today is a wonderfull day";
+
+
     }
 }
